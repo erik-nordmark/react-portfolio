4d2624c36e8c789ee387aaa5d003d2a0c79b2a7d
Erik Nordmark <eriknordmark@Eriks-MacBook-Pro.local>
2018-12-03 11:57:21 +0100

    Temp removed footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './App.scss';
 
-import { Footer } from "./components/Footer/Footer";
+// import { Footer } from "./components/Footer/Footer";
 import { Header } from "./components/Header/Header";
 import { ProjectGrid } from "./components/ProjectGrid/ProjectGrid";
 import { Resume } from "./components/Resume/Resume";
@@ -15,7 +15,7 @@ class App extends Component {
         <Splash quote="Perfection is achieved, not when there is nothing more to add, but when there is nothing left to take away." author="Antoine de Saint-Exupéry" />
         <ProjectGrid />
         <Resume />
-        <Footer text="this is a footer" />
+        {/* <Footer text="this is a footer" /> */}
       </div>
     );
   }
